d0bdaf22c58f31d8ca650182435188636eee5ef9
John <johnsunday803@gmail.com>
2025-01-05 15:15:42 +0100

    New Commit

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -121,7 +121,6 @@ export default function SignIn() {
         </button>
 
         <h1 className="text-3xl font-bold text-center flex-1">Sign In</h1>
-        <h1 className="text-3xl font-bold text-center flex-1">School</h1>
       </div>
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
